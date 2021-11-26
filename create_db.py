883e2fda38fbfe6b2f23b6d35ee3e54d96b425fc
InnoZhan <zhandos1609@gmail.com>
2021-11-26 16:31:25 +0300

    first commit

--- create_db.py
+++ create_db.py
@@ -4,14 +4,14 @@ con = sqlite3.connect('barber.db')
 
 cur = con.cursor()
 
-# cur.execute('''CREATE TABLE feedback
-#                (id INTEGER PRIMARY KEY, employee TEXT, rate REAL, service TEXT, phone TEXT, feedback TEXT, allow TEXT, timedate TEXT)''')
+cur.execute('''CREATE TABLE feedback
+               (id INTEGER PRIMARY KEY, employee TEXT, rate REAL, service TEXT, phone TEXT, feedback TEXT, allow TEXT, timedate TEXT)''')
 
 # cur.execute('DROP TABLE feedback')
 
-rows = cur.execute("SELECT * FROM feedback")
+# rows = cur.execute("SELECT * FROM feedback")
 
-for row in rows:
-    print(row)
+# for row in rows:
+#     print(row)
 
 con.commit()
