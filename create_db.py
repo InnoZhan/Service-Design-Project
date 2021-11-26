16db016b6525986f0bd94c17286af0894c59d48d
InnoZhan <zhandos1609@gmail.com>
2021-11-26 14:49:49 +0300

    first commit

---
+++ create_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+con = sqlite3.connect('barber.db')
+
+cur = con.cursor()
+
+# cur.execute('''CREATE TABLE feedback
+#                (id INTEGER PRIMARY KEY, employee TEXT, rate REAL, service TEXT, phone TEXT, feedback TEXT, allow TEXT, timedate TEXT)''')
+
+# cur.execute('DROP TABLE feedback')
+
+rows = cur.execute("SELECT * FROM feedback")
+
+for row in rows:
+    print(row)
+
+con.commit()
---
+++ server.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import render_template, request, jsonify
+from datetime import datetime
+
+import json 
+import sqlite3
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/sendFeedback", methods=['POST'])
+def save_feedback():
+    con = sqlite3.connect('barber.db')
+    cur = con.cursor()
+    temp = dict(request.form)
+    employee = "Zhandos"
+    service = "Haircut"
+    phone = "89876543210"
+    now = datetime.now()
+    date = now.strftime("%d/%m/%Y %H:%M:%S")
+    cur.execute(f"INSERT INTO feedback (employee, rate, service, phone, feedback, allow, timedate) VALUES ('{employee}', '{temp['rate']}', '{service}', '{phone}', '{temp['feedback']}', '{temp['allow']}', '{date}')")
+    con.commit()
+    return jsonify(isError= False,
+                    message= "Success",
+                    statusCode= 200), 200
+
+
+@app.route("/feedback/<id>")
+def feedback_page(id):
+    return render_template("feedback.html")
+
+
+@app.route("/getFeedback")
+def get_feedback():
+    con = sqlite3.connect('barber.db')
+    cur = con.cursor()
+    rows = cur.execute("SELECT * FROM feedback ORDER BY id DESC")
+    result = []
+    for row in rows:
+        result.append(row)
+
+    return render_template("feedbacks.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
